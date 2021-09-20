e5446b049243602f11d1db787a8361998656981d
SAJID LATIF <indus1530@gmail.com>
2021-09-20 17:53:42 +0500

    SectionA3Activity added

---
+++ app/src/main/java/edu/aku/hassannaqvi/bfwfs20210920/ui/sections/SectionA3Activity.java
@@ -0,0 +1,14 @@
+package edu.aku.hassannaqvi.bfwfs20210920.ui.sections;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SectionA3Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_section_a3);
+    }
+}
