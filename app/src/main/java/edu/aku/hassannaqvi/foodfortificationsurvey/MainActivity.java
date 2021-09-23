b954906b7d5c124e40f29511819c7dbdc92827d7
SAJID LATIF <indus1530@gmail.com>
2021-09-23 16:27:45 +0500

    activity_section_a31.xml someChanges

--- app/src/main/java/edu/aku/hassannaqvi/foodfortificationsurvey/MainActivity.java
+++ app/src/main/java/edu/aku/hassannaqvi/foodfortificationsurvey/MainActivity.java
@@ -21,6 +21,7 @@ import edu.aku.hassannaqvi.foodfortificationsurvey.ui.lists.FormsReportPending;
 import edu.aku.hassannaqvi.foodfortificationsurvey.ui.sections.SectionA1Activity;
 import edu.aku.hassannaqvi.foodfortificationsurvey.ui.sections.SectionA2Activity;
 import edu.aku.hassannaqvi.foodfortificationsurvey.ui.sections.SectionA31Activity;
+import edu.aku.hassannaqvi.foodfortificationsurvey.ui.sections.SectionA32Activity;
 import edu.aku.hassannaqvi.foodfortificationsurvey.ui.sections.SectionB1Activity;
 import edu.aku.hassannaqvi.foodfortificationsurvey.ui.sections.SectionC1Activity;
 import edu.aku.hassannaqvi.foodfortificationsurvey.ui.sections.SectionC2Activity;
@@ -89,16 +90,16 @@ public class MainActivity extends AppCompatActivity {
                 break;
             case R.id.seca1:
                 MainApp.form = new Form();
-                startActivity(new Intent(this, SectionA1Activity.class));
+                startActivity(new Intent(this, SectionA2Activity.class));
                 break;
             case R.id.seca2:
                 MainApp.form = new Form();
-                startActivity(new Intent(this, SectionA2Activity.class));
+                startActivity(new Intent(this, SectionA31Activity.class));
                 break;
 
             case R.id.seca3:
                 MainApp.form = new Form();
-                startActivity(new Intent(this, SectionA31Activity.class));
+                startActivity(new Intent(this, SectionA32Activity.class));
                 break;
             case R.id.secb1:
                 MainApp.form = new Form();
