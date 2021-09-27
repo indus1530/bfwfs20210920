51fcbc12ba227d7dee0fc64e133978b33877e39f
SAJID LATIF <indus1530@gmail.com>
2021-09-27 14:08:43 +0500

    activity_section_c1.xml skipC1071

--- app/src/main/java/edu/aku/hassannaqvi/foodfortificationsurvey/models/Form.java
+++ app/src/main/java/edu/aku/hassannaqvi/foodfortificationsurvey/models/Form.java
@@ -3111,6 +3111,8 @@ public class Form extends BaseObservable implements Observable {
 
     public void setC1071(String c1071) {
         this.c1071 = c1071;
+        setC1072(c1071.equals("1") ? this.c1072 : "");
+        setC1073(c1071.equals("1") ? this.c1073 : "");
         notifyPropertyChanged(BR.c1071);
     }
 
